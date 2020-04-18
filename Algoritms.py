8b59c2eb1ac18300a49ad51abaea6f010c8a9ca0
JuanCJC <juancchaparro97@gmail.com>
2020-04-18 16:16:07 -0500

    sistema funcional manual

---
+++ Algoritms.py
@@ -0,0 +1,10 @@
+
+
+
+#recibe el grid y la serpiente donde quiere calcular la ruta, el grid para saber cuales paredes tiene y la snake para saber el cuerpo
+
+
+#averiguar en que direccion va la serpiente y no tomar ese nodo para evaluar..
+
+def dijkstra(grid,snake):
+    pass
